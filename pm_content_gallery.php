432b5f803aa9749fe281fe81b649c43fba4903f2
Uziel Almeida Oliveira <uzielweb@gmail.com>
2024-11-30 04:42:39 -0300

    Update pm_content_gallery.php

--- pm_content_gallery.php
+++ pm_content_gallery.php
@@ -108,11 +108,12 @@ public function cropImage($file, $size)
     {
         if (version_compare(JVERSION, '4.0', '>=')) {
             $wa = Factory::getDocument()->getWebAssetManager();
-            if (!$wa->assetExists('script', 'jquery')) {
-                $wa->registerAndUseScript('jquery', Uri::root(true) . 'media/vendor/jquery/js/jquery.min.js', ['version' => 'auto', 'relative' => true]);
+          
+            if (!$wa->assetExists('script', 'joomla.jquery')) {
+                $wa->registerAndUseScript('joomlajquery', Uri::root(true) . 'media/vendor/jquery/js/jquery.min.js', ['version' => 'auto', 'relative' => true]);
             }
-            if (!$wa->assetExists('script', 'jquery-migrate')) {
-                $wa->registerAndUseScript('jquery-migrate', Uri::root(true) . 'media/vendor/jquery-migrate/js/jquery-migrate.min.js', ['version' => 'auto', 'relative' => true]);
+            if (!$wa->assetExists('script', 'joomla.jquery-migrate')) {
+                $wa->registerAndUseScript('joomla.jquery-migrate', Uri::root(true) . 'media/vendor/jquery-migrate/js/jquery-migrate.min.js', ['version' => 'auto', 'relative' => true]);
             }
               // check if exists bootstrap in the template and if not, load it
             if (!$wa->assetExists('script', 'bootstrap')) {
@@ -127,18 +128,24 @@ public function cropImage($file, $size)
             if (!$wa->assetExists('style', 'bootstrap.css')) {
                 $wa->registerAndUseStyle('bootstrap.css', 'https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css');
             }
-            
+            if ($this->params->get("gallery_type", "owl_carousel") == "owl_carousel") {   
             $wa->registerAndUseStyle('pm_content_gallery_default', Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/owl.theme.default.min.css', ['version' => 'auto', 'relative' => true]);
             $wa->registerAndUseStyle('pm_content_gallery_carousel', Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/owl.carousel.min.css', ['version' => 'auto', 'relative' => true]);
-            $wa->registerAndUseStyle('pm_content_gallery_temabasico', Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/temabasico.css', ['version' => 'auto', 'relative' => true]);
             $wa->registerAndUseScript('pm_content_gallery_js', Uri::root(true) . 'plugins/content/pm_content_gallery/assets/js/owl.carousel.min.js', ['version' => 'auto', 'relative' => true], ['defer' => true]);
+            }
+            $wa->registerAndUseStyle('pm_content_gallery_temabasico', Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/temabasico.css', ['version' => 'auto', 'relative' => true]);
         } else {
             $doc = Factory::getDocument();
+         
+            if ($this->params->get("gallery_type", "owl_carousel") == "owl_carousel") {             
             $doc->addStyleSheet(Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/owl.theme.default.min.css');
-            $doc->addStyleSheet(Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/owl.carousel.min.css');
+            $doc->addStyleSheet(Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/owl.carousel.min.css');            
+            }
             $doc->addStyleSheet(Uri::root(true) . 'plugins/content/pm_content_gallery/assets/css/temabasico.css');
+            if ($this->params->get("gallery_type", "owl_carousel") == "owl_carousel") {  
             HTMLHelper::_('jquery.framework', true, true);
             $doc->addScript(Uri::root(true) . 'plugins/content/pm_content_gallery/assets/js/owl.carousel.min.js', ['version' => 'auto', 'relative' => true], ['defer' => true]);
+            }
         }
         preg_match_all('@{' . $this->params->get("customtagname", "pmgallery") . '}(.*){/' . $this->params->get("customtagname", "pmgallery") . '}@Us', $article->text, $matches);
         $allMatches = $matches[0];
@@ -158,7 +165,43 @@ public function cropImage($file, $size)
             }
             $html[$m] = '';
             $html[$m] .= '<section class="pmcontentgallery gallery-'.$article->id.$m.'">';
-            $html[$m] .= '<div class="' . ($this->params->get("gallery_type", "owl_carousel") == "owl_carousel" ? 'owl-carousel carrossel-' . $article->id . $m . ' owl-theme' : 'row') . '">';
+           // Obtem o tipo de galeria
+$galleryType = $this->params->get("gallery_type", "owl_carousel");
+
+// Inicializa a variável de classes e atributos
+$carouselClass = '';
+$carouselAttributes = '';
+
+// Configurações baseadas no tipo de galeria
+switch ($galleryType) {
+    case "owl_carousel":
+        $carouselClass = 'owl-carousel carrossel-' . $article->id . $m . ' owl-theme';
+        break;
+
+    case "bootstrap_carousel":
+        $carouselClass = 'carousel slide';
+        $carouselAttributes = ' id="carousel-' . $article->id . $m . '" data-bs-ride="carousel"';
+        break;
+
+    default:
+        $carouselClass = 'row';
+        break;
+}
+
+// Monta o HTML principal
+$html[$m] .= '<div class="' . $carouselClass . '"' . $carouselAttributes . '>';
+
+// Adiciona a div carousel-inner apenas para bootstrap_carousel
+if ($galleryType == "bootstrap_carousel") {
+    $html[$m] .= '<div class="carousel-indicators">';
+foreach ($files as $k => $file) {
+    // Corrigido o índice para o indicador de slide
+    $html[$m] .= '<button type="button" data-bs-target="#carousel-' . $article->id . $m . '" data-bs-slide-to="' . $k-2 . '" ' . ($k == 2 ? 'class="active" aria-current="true"' : '') . ' aria-label="Slide ' . ($k-1) . '"></button>';
+}
+$html[$m] .= '</div>';
+$html[$m] .= '<div class="carousel-inner">';
+
+}
             foreach ($files as $k => $file) {
                 $imagem = Uri::base() . $directory . '/' . $file;
                 $heightb4 = $this->params->get("height", "16by9");
@@ -176,7 +219,7 @@ public function cropImage($file, $size)
                 $imageRatio = $imageWidth / $imageHeight;
                 $imageOrientation = $imageRatio > 1 ? 'landscape' : 'portrait';
                 // generate thumbnail image
-                $thumbnail = $directory . '/thumbnail/' . $file;
+                $thumbnail = $directory . '/thumbnail/' . pathinfo($file, PATHINFO_FILENAME) . '_' . $thumbnailWidth . 'x' . $thumbnailHeight . '.' . pathinfo($file, PATHINFO_EXTENSION);
                 if (!file_exists($directory . '/thumbnail')) {
                     mkdir($directory . '/thumbnail', 0777, true);
                 }
@@ -191,15 +234,15 @@ public function cropImage($file, $size)
                     Factory::getApplication()->enqueueMessage(Text::sprintf('PLG_CONTENT_PM_CONTENT_GALLERY_DIRECTORY_NOT_FOUND', $directory), 'error');
                     continue;
                 }
-                $html[$m] .= '<div class="item '.($this->params->get("gallery_type", "owl_carousel") == "owl_carousel" ? 'item-'.$article->id.$m.'-'.$k : 'col-md-'.round(12/$this->params->get("images_per_row")).'').' mb-3">';
+                $html[$m] .= '<div class="' . ($galleryType == "bootstrap_carousel" ? 'carousel-item ' . ($k == 2 ? 'active' : '') : 'item ' . ($galleryType == "owl_carousel" ? 'item-' . $article->id . $m . '-' . $k : 'col-md-' . round(12 / $this->params->get("images_per_row")) . ' mb-3')) . '">';
                 $html[$m] .= '<div class="overflow-hidden embed-responsive embed-responsive-' . $heightb4 . ' ratio ratio-' . $heightb5 . ' ' . $imageOrientation . '">';
                 if ($this->params->get("modal", "1") == "1") {
                     $html[$m] .= '<div class="modal-toggle" data-bs-toggle="modal" data-bs-target="#galleryModal-' . $article->id . '-' . $m . '-' . $k . '" rel="gallery-' . $article->id . '-' . $m . '" gallery="' . $article->id . '-' . $m . '">';
                 }
                 if ($this->params->get("gallery_type", "owl_carousel") == "grid") {
-                    $html[$m] .= HTMLHelper::_('image', $thumbnail, $alt, ['class' => 'embed-responsive-item img-fluid lazyload', 'loading' => 'lazy']);
+                    $html[$m] .= HTMLHelper::_('image', $thumbnail, $alt, ['class' => 'embed-responsive-item img-fluid']);
                 } else {
-                    $html[$m] .= HTMLHelper::_('image', $imagem, $alt, ['class' => 'embed-responsive-item img-fluid lazyload', 'loading' => 'lazy']);
+                    $html[$m] .= HTMLHelper::_('image', $imagem, $alt, ['class' => 'embed-responsive-item img-fluid']);
                 }
                   if ($this->params->get("modal", "1") == "1") {
                     $html[$m] .= '</div>';
@@ -207,9 +250,22 @@ public function cropImage($file, $size)
                 $html[$m] .= '</div>';
                 $html[$m] .= '</div>';
             }
+        
+            if ($this->params->get("gallery_type", "owl_carousel") == "bootstrap_carousel") {
+                $html[$m] .= '</div>';
+                $html[$m] .= '<button class="carousel-control-prev" type="button" data-bs-target="#carousel-' . $article->id . $m . '" data-bs-slide="prev">';
+                $html[$m] .= '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+                $html[$m] .= '<span class="visually-hidden">Previous</span>';
+                $html[$m] .= '</button>';
+                $html[$m] .= '<button class="carousel-control-next" type="button" data-bs-target="#carousel-' . $article->id . $m . '" data-bs-slide="next">';
+                $html[$m] .= '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+                $html[$m] .= '<span class="visually-hidden">Next</span>';
+                $html[$m] .= '</button>';
+            }
             $html[$m] .= '</div>';
             $html[$m] .= '<div class="description">' . $descricao . '</div>';
             $html[$m] .= '</section>';
+            if ($this->params->get("gallery_type", "owl_carousel") == "owl_carousel") {  
             $html[$m] .= '<script>
                 jQuery(document).ready(function($){
                     $(".carrossel-'.$article->id.$m.'").owlCarousel({
@@ -234,10 +290,20 @@ public function cropImage($file, $size)
                     });
                 });
             </script>';
+            }
+            elseif ($this->params->get("gallery_type", "owl_carousel") == "bootstrap_carousel") {
+                $html[$m] .= '<script>
+                jQuery(document).ready(function($){
+                    $("#carousel-' . $article->id . $m . '").carousel({
+                        interval: ' . $this->params->get("interval", "5000") . ',
+                    });
+                });
+            </script>';
+            }
             // Append modals HTML to the end of article text
             $article->text = preg_replace('@{' . $this->params->get("customtagname", "pmgallery") . '}(.*){/' . $this->params->get("customtagname", "pmgallery") . '}@Us', $html[$m], $article->text, 1);
         }
-        $k = 0;
+     
         if ($this->params->get("modal", "1") == "1") {
             
             $modalsHtml = '<div class="pm-modal-gallery pm-modal-gallery-' . $article->id .' position-relative">';
@@ -259,7 +325,7 @@ public function cropImage($file, $size)
             $modalsHtml .= '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="fas fa-times"></i></button>';
             $modalsHtml .= '</div>';            
             $modalsHtml .= '<div class="modal-body position-relative">';
-            $modalsHtml .= HTMLHelper::_('image', $imagem, $alt, ['class' => 'img-fluid lazyload', 'loading' => 'lazy']);
+            $modalsHtml .= HTMLHelper::_('image', $imagem, $alt, ['class' => 'img-fluid']);
             // Botão Previous
             $modalsHtml .= '<button class="btn btn-secondary btn-prev" data-bs-target="#galleryModal-' . $article->id . '-' . $m . '-' . ($k - 1) . '" data-bs-toggle="modal" ' . ($k == 2 ? 'disabled' : '') . '><i class="fas fa-chevron-left"></i></button>';
             // Botão Next
